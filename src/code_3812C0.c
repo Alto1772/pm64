6e68978d5febc8a738a5606897669926f0b86f45
Alto1772 <56553686+Alto1772@users.noreply.github.com>
2021-02-02 00:20:26 +0800

    c3812c0-e008042c

--- src/code_3812C0.c
+++ src/code_3812C0.c
@@ -10,6 +10,8 @@ INCLUDE_ASM(s32, "code_3812C0", func_E00801D0);
 void func_E0080424(void){
 }
 
-INCLUDE_ASM(s32, "code_3812C0", func_E008042C);
+void func_E008042C(void) {
+    func_E0080448();
+}
 
 INCLUDE_ASM(s32, "code_3812C0", func_E0080448);
