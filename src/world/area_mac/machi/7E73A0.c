c68a7d3055f390e031da51e7dccc631829a7548b
Alto1772 <56553686+Alto1772@users.noreply.github.com>
2021-02-06 19:00:51 +0800

    cworld-area_mac-machi-7e73a0-80240030
    ApiStatus ???

--- src/world/area_mac/machi/7E73A0.c
+++ src/world/area_mac/machi/7E73A0.c
@@ -2,7 +2,10 @@
 
 INCLUDE_ASM(s32, "world/area_mac/machi/7E73A0", func_80240000_7E73A0);
 
-INCLUDE_ASM(s32, "world/area_mac/machi/7E73A0", func_80240030_7E73D0);
+// ApiStatus ???
+s32 func_80240030_7E73D0(void) {
+    return 2;
+}
 
 INCLUDE_ASM(s32, "world/area_mac/machi/7E73A0", func_80240038_7E73D8);
 
