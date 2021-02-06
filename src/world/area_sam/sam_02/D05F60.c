44b4d630f2b75d758983b25fb3434fccbc51d78c
Alto1772 <56553686+Alto1772@users.noreply.github.com>
2021-02-06 19:21:38 +0800

    cworld-area_sam-sam_02-d05f60-80240b24
    ApiStatus ???

--- src/world/area_sam/sam_02/D05F60.c
+++ src/world/area_sam/sam_02/D05F60.c
@@ -20,7 +20,10 @@ INCLUDE_ASM(s32, "world/area_sam/sam_02/D05F60", func_80240A44_D06174);
 
 #include "world/common/GetPartnerCall800EB168.inc.c"
 
-INCLUDE_ASM(s32, "world/area_sam/sam_02/D05F60", func_80240B24_D06254);
+// ApiStatus ???
+s32 func_80240B24_D06254(void) {
+    return 2;
+}
 
 INCLUDE_ASM(s32, "world/area_sam/sam_02/D05F60", func_80240B2C_D0625C);
 
