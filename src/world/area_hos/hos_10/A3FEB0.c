4cd703953b4d4a1d0db2cf4b342eb07ec10f58da
Alto1772 <56553686+Alto1772@users.noreply.github.com>
2021-04-01 14:44:42 +0800

    cworld-area_hos-hos_10-a3feb0-80240bec

--- src/world/area_hos/hos_10/A3FEB0.c
+++ src/world/area_hos/hos_10/A3FEB0.c
@@ -8,6 +8,9 @@ INCLUDE_ASM(s32, "world/area_hos/hos_10/A3FEB0", func_80240B44_A40284);
 
 INCLUDE_ASM(s32, "world/area_hos/hos_10/A3FEB0", func_80240B98_A402D8);
 
-INCLUDE_ASM(s32, "world/area_hos/hos_10/A3FEB0", func_80240BEC_A4032C);
+ApiStatus func_80240BEC_A4032C(void) {
+    gPlayerData.currentPartner = PARTNER_NONE;
+    return ApiStatus_DONE2;
+}
 
 INCLUDE_ASM(s32, "world/area_hos/hos_10/A3FEB0", func_80240BFC_A4033C);
