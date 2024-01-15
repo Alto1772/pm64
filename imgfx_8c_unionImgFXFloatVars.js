2b8b6a5ada749c9412c4659f611acb958f70bfe3
Alto1772 <Alto1772@users.noreply.github.com>
2024-01-15 15:07:16 +0000

    deploy: 4fdd49c6ef51612b08b702cd95fd63b2064d9749

---
+++ imgfx_8c_unionImgFXFloatVars.js
@@ -0,0 +1,7 @@
+var imgfx_8c_unionImgFXFloatVars =
+[
+    [ "anim", "imgfx_8c.html#a6f5622330705b2bc21ea3e4eafa31a08", null ],
+    [ "overlay", "imgfx_8c.html#ab0e37a5904a4f8af344bec70cd3c9e8e", null ],
+    [ "raw", "imgfx_8c.html#abe83327cd7c09513e3d99934c2e98e71", null ],
+    [ "wavy", "imgfx_8c.html#a2c401cd3da7ea693b4ec86c087ad3eb8", null ]
+];
---
+++ imgfx_8c_unionImgFXIntVars.js
@@ -0,0 +1,10 @@
+var imgfx_8c_unionImgFXIntVars =
+[
+    [ "anim", "imgfx_8c.html#a7758518d25fe6d8088b2bcfe729ce15c", null ],
+    [ "args", "imgfx_8c.html#a2c44e6e4f55e4639fb118857713d81f1", null ],
+    [ "color", "imgfx_8c.html#a0c518ac71bcdde1b81e5842a4987f524", null ],
+    [ "hologram", "imgfx_8c.html#a66a1b59b0f67bdcfa60877e4efa42cf0", null ],
+    [ "overlay", "imgfx_8c.html#a23d3badaa836e20358d533941872d563", null ],
+    [ "raw", "imgfx_8c.html#a903fa6db05540cd91a99ef398343ff88", null ],
+    [ "wavy", "imgfx_8c.html#a8e5a2996ec6b851d3419c56619952ac2", null ]
+];
