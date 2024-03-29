32b4eec752670a3068294afa5064a4015f7bebae
AltoXorg <atrl101@yahoo.com>
2024-03-29 12:06:44 +0800

    starpoint gfx

--- src/starpoint.c
+++ src/starpoint.c
@@ -6,56 +6,18 @@
 INCLUDE_IMG("starpoint/starpoint.png", D_802A1018_7A89B8);
 #include "starpoint/load_starpoint.gfx.inc.c"
 #include "starpoint/starpoint.vtx.inc.c"
-Gfx D_802A20D0_7A9A70[] = {
-    gsSPSetLights1(starpoint_lights1),
-    gsDPPipeSync(),
-    gsDPSetCycleType(G_CYC_1CYCLE),
-    gsDPSetRenderMode(G_RM_XLU_SURF, G_RM_XLU_SURF2),
-    gsSPDisplayList(D_802A2018_7A99B8),
-    gsSPClearGeometryMode(G_LIGHTING),
-    gsSPSetGeometryMode(G_CULL_BACK | G_SHADING_SMOOTH),
-    gsSPVertex(D_802A2090_7A9A30, 4, 0),
-    gsSP2Triangles(0, 1, 2, 0, 0, 2, 3, 0),
-    gsSPEndDisplayList(),
-};
-Gfx D_802A2130_7A9AD0[] = {
-    gsSPSetLights1(starpoint_lights1),
-    gsSPDisplayList(D_802A20D0_7A9A70),
-    gsSPEndDisplayList(),
-};
-Gfx D_802A2158_7A9AF8[] = {
-    gsSPSetLights1(starpoint_lights1),
-    gsSPDisplayList(D_802A2130_7A9AD0),
-    gsSPEndDisplayList(),
-};
+#include "starpoint/D_802A20D0_7A9A70.gfx.inc.c"
+#include "starpoint/D_802A2130_7A9AD0.gfx.inc.c"
+#include "starpoint/D_802A2158_7A9AF8.gfx.inc.c"
 
 #include "starpoint/starpoint_lights2.light.inc.c"
 #include "starpoint/starpoints.vtx.inc.c"
 INCLUDE_IMG("starpoint/starpoints.png", D_802A21D8_7A9B78);
 #include "starpoint/load_starpoints.gfx.inc.c"
 
-Gfx D_802A3250_7AABF0[] = {
-    gsSPSetLights1(starpoint_lights2),
-    gsDPPipeSync(),
-    gsDPSetCycleType(G_CYC_1CYCLE),
-    gsDPSetRenderMode(G_RM_XLU_SURF, G_RM_XLU_SURF2),
-    gsSPDisplayList(D_802A31D8_7AAB78),
-    gsSPClearGeometryMode(G_LIGHTING),
-    gsSPSetGeometryMode(G_CULL_BACK | G_SHADING_SMOOTH),
-    gsSPVertex(D_802A2198_7A9B38, 4, 0),
-    gsSP2Triangles(0, 1, 2, 0, 0, 2, 3, 0),
-    gsSPEndDisplayList(),
-};
-Gfx D_802A32B0_7AAC50[] = {
-    gsSPSetLights1(starpoint_lights2),
-    gsSPDisplayList(D_802A3250_7AABF0),
-    gsSPEndDisplayList(),
-};
-Gfx D_802A32D8_7AAC78[] = {
-    gsSPSetLights1(starpoint_lights2),
-    gsSPDisplayList(D_802A32B0_7AAC50),
-    gsSPEndDisplayList(),
-};
+#include "starpoint/D_802A3250_7AABF0.gfx.inc.c"
+#include "starpoint/D_802A32B0_7AAC50.gfx.inc.c"
+#include "starpoint/D_802A32D8_7AAC78.gfx.inc.c"
 
 #include "starpoint/digit_0.vtx.inc.c"
 INCLUDE_IMG("starpoint/digit_0.png", D_802A3340_7AACE0);
